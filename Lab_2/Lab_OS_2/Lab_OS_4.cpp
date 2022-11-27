967f2db22d71d50b63eef547159c7898a451a5f9
Stas012 <99864736+Stas012@users.noreply.github.com>
2022-11-27 12:43:50 +0300

    Add files via upload

---
+++ Lab_2/Lab_OS_2/Lab_OS_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 3, c = 3;
+
+    for (int i = 0; i < 1000000000; i++) {
+        a += 2*b + c - i;
+    }
+
+    cout << a << endl;
+    return 0;
+}
